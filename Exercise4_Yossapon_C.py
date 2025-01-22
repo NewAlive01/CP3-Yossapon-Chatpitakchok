64913cc91863064a50068a160737e9527d53756b
NewAlive01 <yossaponasc@gmail.com>
2025-01-22 11:16:02 +0700

    Create Exercise4_Yossapon_C.py

---
+++ Exercise4_Yossapon_C.py
@@ -0,0 +1,7 @@
+print("--Enter Your Score--")
+FE  = float(input("Foundation English :"))
+GB  = float(input("General Business :"))
+ICS = float(input("Introduction to Computer Systems :"))
+CP  = float(input("Computer Programming :"))
+
+print(f"--YourScore--\nFoundation English :{FE}\nGeneral Business :{GB}\nIntroduction to Computer Systems :{ICS}\nComputer Programming :{CP}")
