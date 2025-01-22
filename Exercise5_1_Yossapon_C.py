2cc27db58ad0db942bf83fe0fb5ca66b98fb6ac5
NewAlive01 <yossaponasc@gmail.com>
2025-01-22 15:41:11 +0700

    Create Exercise5_1_Yossapon_C.py

---
+++ Exercise5_1_Yossapon_C.py
@@ -0,0 +1,7 @@
+print("ยิดดีต้อนรับสู่โปรแกรมเครื่องคิดเลขแบบง่ายๆ")
+a = int(input("input1 :"))
+b = int(input("input2 :"))
+print(a,"+",b,"=",(a+b))
+print(a,"-",b,"=",(a-b))
+print(a,"*",b,"=",(a*b))
+print(a,"/",b,"=",(a/b))
